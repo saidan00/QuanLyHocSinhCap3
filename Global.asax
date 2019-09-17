592fcc54cfa68a812332beb794925438e84f4e8e
JaySGH <huysgh94@gmail.com>
2019-09-17 18:23:00 +0700

    asp.net mvc framework

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuanLyHocSinhCap3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyHocSinhCap3.Startup))]
+namespace QuanLyHocSinhCap3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
