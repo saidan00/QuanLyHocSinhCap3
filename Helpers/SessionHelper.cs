927f600be39a76e69caa93d528924f9eeb3a132f
JaySGH <huysgh94@gmail.com>
2019-09-27 22:52:28 +0700

    remove token-based login, replace by session based-login, add migratons, install cryptoHeler for hasing Password

---
+++ Helpers/SessionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyHocSinhCap3.Helpers
+{
+    public class SessionHelper
+    {
+        public bool IsLoggedIn()
+        {
+            if (HttpContext.Current.Session["userId"] == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
