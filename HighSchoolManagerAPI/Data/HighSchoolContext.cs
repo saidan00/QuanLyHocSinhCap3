c796b1f3e7ccb52f5b09608754fb5ed4e40758ef
JaySGH <huysgh94@gmail.com>
2019-10-09 18:44:12 +0700

    Adding authentication & authorization, re-setup source

--- HighSchoolManagerAPI/Data/HighSchoolContext.cs
+++ HighSchoolManagerAPI/Data/HighSchoolContext.cs
@@ -1,5 +1,3 @@
-using IdentityServer4.EntityFramework.Options;
-using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
