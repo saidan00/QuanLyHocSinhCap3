1b0f0daeb05a0ada2185dba730affd2005011c1c
JaySGH <huysgh94@gmail.com>
2019-10-10 12:06:12 +0700

    studentApiController

--- HighSchoolManagerAPI/Controllers/AccountController.cs
+++ HighSchoolManagerAPI/Controllers/AccountController.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HighSchoolManagerAPI.Models;
+using HighSchoolManagerAPI.FrontEndModels;
 using HighSchoolManagerAPI.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
